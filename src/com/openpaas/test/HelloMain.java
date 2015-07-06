0aa03c6c6445d6a8c005f6fcf1f5a12f8adabb3e
yihyunyi <yihyunyi@bluedigm.com>
2015-07-06 13:39:56 +0900

    first commit

---
+++ src/com/openpaas/test/HelloMain.java
@@ -0,0 +1,21 @@
+package com.openpaas.test;
+
+import java.util.Map;
+
+public class HelloMain {
+
+	public static void main(String[] args) {
+		String sVcap = "no";
+	    
+		System.out.println("System Environment:");
+
+		for (Map.Entry<String, String> envvar : System.getenv().entrySet()) {
+	        System.out.println(envvar.getKey() + ": " + envvar.getValue());
+	        
+	        if(envvar.getKey().equalsIgnoreCase("VCAP_APPLICATION"))
+	        	sVcap = envvar.getValue();
+	    }
+		
+		System.out.println("VCAP_APPLICATION: " + sVcap);
+	}
+}
