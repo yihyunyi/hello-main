f37c928c7465968a6c569823694ea9f328d925ab
yihyunyi <yihyunyi@bluedigm.com>
2016-02-23 16:15:50 +0900

    Update HelloMain.java

--- src/com/openpaas/test/HelloMain.java
+++ src/com/openpaas/test/HelloMain.java
@@ -7,7 +7,7 @@ public class HelloMain {
 	public static void main(String[] args) {
 		String sVcap = "no";
 	    
-		System.out.println("System Environment:");
+#		System.out.println("System Environment:");
 
 		for (Map.Entry<String, String> envvar : System.getenv().entrySet()) {
 	        System.out.println(envvar.getKey() + ": " + envvar.getValue());
@@ -16,6 +16,6 @@ public class HelloMain {
 	        	sVcap = envvar.getValue();
 	    }
 		
-#		System.out.println("VCAP_APPLICATION: " + sVcap);
+		System.out.println("VCAP_APPLICATION: " + sVcap);
 	}
 }
