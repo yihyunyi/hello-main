ab96d1ac0c899ccf7512accbe1ace3581d50444f
yihyunyi <yihyunyi@bluedigm.com>
2016-02-23 15:58:29 +0900

    Update HelloMain.java
    sysout block

--- src/com/openpaas/test/HelloMain.java
+++ src/com/openpaas/test/HelloMain.java
@@ -7,15 +7,15 @@ public class HelloMain {
 	public static void main(String[] args) {
 		String sVcap = "no";
 	    
-		System.out.println("System Environment:");
+#		System.out.println("System Environment:");
 
 		for (Map.Entry<String, String> envvar : System.getenv().entrySet()) {
-	        System.out.println(envvar.getKey() + ": " + envvar.getValue());
+#	        System.out.println(envvar.getKey() + ": " + envvar.getValue());
 	        
 	        if(envvar.getKey().equalsIgnoreCase("VCAP_APPLICATION"))
 	        	sVcap = envvar.getValue();
 	    }
 		
-		System.out.println("VCAP_APPLICATION: " + sVcap);
+#		System.out.println("VCAP_APPLICATION: " + sVcap);
 	}
 }
